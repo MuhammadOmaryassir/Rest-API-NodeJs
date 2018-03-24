780e1a103558a02556a0c9e0d939a1c2f93ff647
Omaroovee <Muhammad.omar.eg@gmail.com>
2018-03-24 19:36:19 +0200

    updating and Deleting Books

--- Routes/bookRoutes.js
+++ Routes/bookRoutes.js
@@ -30,16 +30,69 @@ routes = function (Book) {
 
         })
 
-    // get one book using the book id 
+    // Book Middleware to find a book by id
+    bookRouter.use('/:bookId', function(req,res,next){
+        Book.findById(req.params.bookId, function(err,book){
+            if(err)
+                res.status(500).send(err)
+            else if(book)
+            {
+                req.book = book
+                next()
+            }
+            else
+            {
+                res.status(404).send('no book found')
+            }
+        });
+    });
     bookRouter.route('/:bookId')
-        .get(function (req, res) {
+    // get all the books in the DB
+        .get(function(req,res){
 
+            res.json(req.book)
 
-            Book.findById(req.params.bookId, function (err, book) {
-                if (err)
+        })
+        // update a specific book
+        .put(function(req,res){
+            req.book.title = req.body.title
+            req.book.author = req.body.author
+            req.book.genre = req.body.genre
+            req.book.read = req.body.read
+            req.book.save(function(err){
+                if(err)
                     res.status(500).send(err)
-                else
-                    res.json(book)
+                else{
+                    res.json(req.book)
+                }
+            })
+        })
+        // update a specific option or element  of the book data
+        .patch(function(req,res){
+            if(req.body._id)
+                delete req.body._id
+
+            for(var p in req.body)
+            {
+                req.book[p] = req.body[p]
+            }
+
+            req.book.save(function(err){
+                if(err)
+                    res.status(500).send(err)
+                else{
+                    res.json(req.book)
+                }
+            })
+        })
+        // delete book from the DB
+        .delete(function(req,res){
+            req.book.remove(function(err){
+                if(err)
+                    res.status(500).send(err)
+                else{
+                    res.status(204).send('Removed')
+                }
             });
         });
 
